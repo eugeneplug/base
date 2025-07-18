de4a17cdfed3d3465d5d596bb904a5defbc54e67
Eugene <68537036+eugeneplug@users.noreply.github.com>
2025-07-18 15:44:38 +0700

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -1,1 +1,11 @@
 
+
+function enqueue_custom_styles() {
+    wp_enqueue_style(
+        'header-style', 
+        get_template_directory_uri() . '/assets/css/style.css' // ← только относительный путь от темы
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+
+
