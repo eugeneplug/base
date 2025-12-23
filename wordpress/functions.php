3e1179f67adb3360e6d06e7add6069104e189fb8
Eugene <68537036+eugeneplug@users.noreply.github.com>
2025-12-23 22:43:05 +0300

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -9,3 +9,34 @@ function enqueue_custom_styles() {
 add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
 
 
+<!-- ОЧЕНЬ ВАЖНО, ДОБАВЛЕНИЕ В ССЫЛКИ В ХЛЕБНЫЕ КРОШКИ + ИЗМЕНЕНИЕ ССЫЛКИ ЧПУ -->
+
+<?php
+                        // Добавляем "Каталог" в хлебные крошки Rank Math для кастомного типа catalog
+add_filter('rank_math/frontend/breadcrumb/items', function ($crumbs, $class) {
+    if (is_singular('catalog')) {
+        array_splice($crumbs, 1, 0, [['Оборудование', '/oborudovanie/']]);
+    }
+    return $crumbs;
+}, 10, 2);
+
+
+//   изменение адрес ссылки каталог детальная
+add_action('init', 'change_c_to_k');
+
+function change_c_to_k()
+{
+    global $wp_rewrite;
+
+    // Меняем slug кастомного типа записи
+    $args = get_post_type_object('catalog');
+    if ($args) {
+        $args->rewrite['slug'] = 'oborudovanie';
+        register_post_type($args->name, (array) $args);
+    }
+
+    // Обновляем правила ЧПУ
+    $wp_rewrite->flush_rules();
+}
+                        
+                        ?>
