a725e4d22d8aa400f5d918365c874949899b8e2b
Eugene <68537036+eugeneplug@users.noreply.github.com>
2026-02-18 17:51:25 +0700

    Create contact_form7.php

---
+++ contact_form7.php
@@ -0,0 +1,9 @@
+add_filter('wpcf7_autop_or_not', '__return_false');
+
+<!-- добавить это в файл фанкш пхп для того чтобы все нормально вставало -->
+
+
+
+
+
+<?php echo do_shortcode('[contact-form-7 id="527d53c" title="Контактная форма 1"]'); ?>
