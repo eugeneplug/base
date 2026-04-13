ed5e1615ffbb6015e85ae8b950e3135f726c6443
Eugene <68537036+eugeneplug@users.noreply.github.com>
2026-04-13 12:53:10 +0300

    Update functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -1,4 +1,5 @@
 
+<?php
 
 function enqueue_custom_styles() {
     wp_enqueue_style(
@@ -11,7 +12,6 @@ add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
 
 <!-- ОЧЕНЬ ВАЖНО, ДОБАВЛЕНИЕ В ССЫЛКИ В ХЛЕБНЫЕ КРОШКИ + ИЗМЕНЕНИЕ ССЫЛКИ ЧПУ -->
 
-<?php
                         // Добавляем "Каталог" в хлебные крошки Rank Math для кастомного типа catalog
 add_filter('rank_math/frontend/breadcrumb/items', function ($crumbs, $class) {
     if (is_singular('catalog')) {
@@ -19,6 +19,51 @@ add_filter('rank_math/frontend/breadcrumb/items', function ($crumbs, $class) {
     }
     return $crumbs;
 }, 10, 2);
+
+
+<!-- ЕТА ШТУКА ДЛЯ ALL IN SEO ПЛАГИНА, ТУТ ДОБАВЛЕНИЕ И УДАЛЕНИЕ СТАРОЙ ФИГНИ -->
+
+add_filter('aioseo_breadcrumbs_trail', function ($crumbs) {
+    if (is_singular('special')) {
+        // 1. Сначала удаляем все элементы с /category/
+        foreach ($crumbs as $index => $crumb) {
+            if (isset($crumb['link']) && strpos($crumb['link'], '/category/') !== false) {
+                unset($crumbs[$index]);
+            }
+        }
+        
+        // 2. Переиндексируем массив после удаления
+        $crumbs = array_values($crumbs);
+        
+        // 3. Проверяем, есть ли уже ссылка на /specialists/
+        $has_specialists_link = false;
+        foreach ($crumbs as $crumb) {
+            if (isset($crumb['link']) && $crumb['link'] === '/specialists/') {
+                $has_specialists_link = true;
+                break;
+            }
+        }
+        
+        // 4. Добавляем ссылку на /specialists/, если её ещё нет
+        if (!$has_specialists_link) {
+            $new_crumb = [
+                'label' => 'Специалисты',
+                'link'  => '/specialists/'
+            ];
+            array_splice($crumbs, 1, 0, [$new_crumb]);
+        }
+    }
+    
+    return $crumbs;
+});
+
+
+
+
+
+
+
+
 
 
 //   изменение адрес ссылки каталог детальная
@@ -38,6 +83,17 @@ function change_c_to_k()
     // Обновляем правила ЧПУ
     $wp_rewrite->flush_rules();
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
